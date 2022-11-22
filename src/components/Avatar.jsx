0bcd8930bd5fbbd5aaf296e4de6f131b907431ce
evelyn <evelynkim.kr@gmail.com>
2022-11-22 20:35:41 +0900

    basic-props

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Avatar({image, isNew}) {
+    return (
+        <div className='avatar'>
+            {isNew && <span className='new'>New</span>}
+            <img className='photo' src={image} alt='avatar'/>
+        </div>
+    );
+}
+
